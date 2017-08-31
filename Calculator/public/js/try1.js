394cae0c25c45f8b2306afa270e88ed6a1c743d3
Grover Sean Reyes <Zilex.Seryth@gmail.com>
2017-08-31 17:29:35 +0800

    Used Eval function

--- Calculator/public/js/try1.js
+++ Calculator/public/js/try1.js
@@ -19,13 +19,15 @@ function btnClick(value){
     if (equalslick) {
         equalslick = false;
         display.value = "";
+        equation.innerHTML = "";
+        arrayInput = [];
     }
     var isnumber = false;
     var isoperator = false;
     var isSpecial = false;
     var isblank = false;
 
-    //check if input is number or operator
+    //check if input is number or operator or special
     if(numbers.includes(parseInt(value))){
         isnumber = true;
     }
@@ -58,22 +60,43 @@ function btnClick(value){
             display.value = "";
             equation.innerHTML = arrayInput.toString().replace(/,/g,'');
         }
+
         //this if is for 1number equations
         if (value == "√") {
-            display.value = calculate(value);
-            addhistory(value);
+            
+            arrayInput.unshift(value);
+            arrayInput.splice(-1,1);
+
+            equation.innerHTML = arrayInput.toString().replace(/,/g,'');
+
+            OneNumCalc(arrayInput[arrayInput.length - 1],value);
+
             equalslick = true;
-            clearAll();
+
         }else if (value == "1⁄x"){
-            display.value = calculate(value);
-            addhistory(value);
+            
+            arrayInput.unshift(value);
+            arrayInput.splice(-1,1);
+
+            equation.innerHTML = arrayInput.toString().replace(/,/g,'');
+
+            label.innerHTML = "";
+
+            OneNumCalc(arrayInput[arrayInput.length - 1],value);
+
             equalslick = true;
-            clearAll();
         }else if (value == "%"){
-            display.value = calculate(value);
-            addhistory(value);
+            
+            arrayInput.unshift(value);
+            arrayInput.splice(-1,1);
+
+            equation.innerHTML = arrayInput.toString().replace(/,/g,'');
+
+            label.innerHTML = "";
+
+            OneNumCalc(arrayInput[arrayInput.length - 1],value);
+
             equalslick = true;
-            clearAll();
         }
     }
     //if user clicks on a Special button
@@ -81,10 +104,8 @@ function btnClick(value){
         if (value == "MC") {
             memory = "";
         }else if(value == "M+"){
-
-             memory = (memory + display.value);
+             memory = (parseFloat(memory) + parseFloat(display.value));
         }else if(value == "MS"){
-
                 memory = display.value;
         }else if(value == "MR"){
 
@@ -102,30 +123,56 @@ function btnClick(value){
                     display.value = display.value.replace(/-/g,'');
                 }
         }else if(value == "="){
-                calculate(arrayInput);
+                arrayInput.push(display.value);
+                equation.innerHTML = arrayInput.toString().replace(/,/g,'');
+                calculate(equation.innerHTML)
         }
     }
 }
 
-function calculate(arraysinput){
+function calculate(inputs){
     
+    var total = eval(inputs.toString().replace(/,/g,''));
+
+    display.value = total;
+
+    equalslick = true;
+
+    addhistory(equation.innerHTML, total);
 }
 
+//calculate a 1 number expression
+function OneNumCalc(inputs, btnvalue){
+    var total;
+    if(btnvalue == "√"){
+       total = Math.sqrt(inputs)
+    }else if(btnvalue == "%"){
+        total = inputs/100;
+    }else if(btnvalue == "1⁄x"){
+        total = 1/inputs
+    }else if(btnvalue == "√"){
+        total = Math.sqrt(inputs)
+    }
+    display.value = total;
+}
+
+
+//clear all including memory
 function clearAll(){
-    num1 = undefined;
-    num2 = undefined;
-    ln1.innerHTML = "First Number:";
-    ln2.innerHTML = "Second Number:";
-    label.innerHTML= "";
+    equation.innerHTML = "";
+    label.innerHTML = "";
+    arrayInput = [];
 }
 
-function addhistory(argument){
+
+//add history
+function addhistory(argument1, argument2){
         var li = document.createElement("li");
-        if (num2 != undefined) {
-            li.appendChild(document.createTextNode(num1+ " " + argument + " " +num2+' = '+ (calculate(argument))));
-        }else {
-            li.appendChild(document.createTextNode(argument+ " " + num1 + ' = '+ (calculate(argument))));
-        }
+        
+            li.appendChild(document.createTextNode(argument1 + " = " + argument2));
+        
+        //    li.appendChild(document.createTextNode(argument+ " " + num1 + ' = '+ (calculate(argument))));
+        
         li.setAttribute("class", "list-group-item"); // added line
         ul.appendChild(li);
 }
